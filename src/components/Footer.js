4f7fc1bd8cc6bff2ec33822e9c1e933bca313f5f
Le Trong Nghia <lenghia211105@gmail.com>
2026-08-12 09:41:28 +0700

    Update footer with student information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,11 @@ function Footer() {
     <footer id="contact" className="student-footer">
       <Container>
         <h2>Student Information</h2>
-        <p><strong>Student Name:</strong> [Student's Full Name]</p>
+        <p><strong>Student Name:</strong> Lê Trọng Nghĩa</p>
         <p><strong>Student ID:</strong> HE190817</p>
-        <p><strong>Class Name:</strong> [Class Name]</p>
-        <p><strong>Email Address:</strong> [Student Email]</p>
+        <p><strong>Class Name:</strong> FER202_M_bL5</p>
+        <p><strong>Email Address:</strong> lenghia211105@gmail.com
+    </p>
       </Container>
     </footer>
   );
